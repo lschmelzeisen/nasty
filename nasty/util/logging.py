63be005b7090069bbcd0c317af9a1de41f378d0a
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2019-09-25 21:12:34 +0200

    Reduce DEBUG logging of third-party modules.

--- nasty/util/logging.py
+++ nasty/util/logging.py
@@ -1,7 +1,10 @@
 import logging
 from logging import getLogger
 
+import tweepy
 import urllib3
+import oauthlib
+import requests_oauthlib
 
 
 def setup_logging(level: str):
@@ -15,5 +18,11 @@ def setup_logging(level: str):
     # Reduce log spam from urllib3 depending on own log level.
     if numeric_level <= logging.DEBUG:
         getLogger(urllib3.__name__).setLevel(logging.INFO)
+        getLogger(tweepy.__name__).setLevel(logging.INFO)
+        getLogger(oauthlib.__name__).setLevel(logging.INFO)
+        getLogger(requests_oauthlib.__name__).setLevel(logging.INFO)
     else:
         getLogger(urllib3.__name__).setLevel(logging.WARN)
+        getLogger(tweepy.__name__).setLevel(logging.WARN)
+        getLogger(oauthlib.__name__).setLevel(logging.WARN)
+        getLogger(requests_oauthlib.__name__).setLevel(logging.WARN)
