280c90ccd1b93e55e1ab77165a47f4a35d64c181
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2019-11-21 20:48:49 +0100

    Update Jobs classes for new retrieval modes.
    There currently are not tests that validate that Jobs.run() works with
    replies or thread jobs, but there is almost no difference to a search
    job there, so I expect this to not be necessary.

--- nasty/jobs.py
+++ nasty/jobs.py
@@ -1,29 +1,159 @@
 import json
 import lzma
 import multiprocessing
+from abc import ABC, abstractmethod
 from datetime import datetime
 from logging import getLogger
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 from uuid import uuid4
 
+from nasty.replies import Replies
 from nasty.search import Search
+from nasty.thread import Thread
+from nasty.timeline import Timeline
 from nasty.util.consts import NASTY_DATE_TIME_FORMAT
 from nasty.util.json import JsonSerializedException
 
 
-class Job:
+class _Work(ABC):
     def __init__(self,
-                 id: str,
-                 query: Search.Query,
-                 max_tweets: int,
-                 batch_size: Optional[int] = None,
-                 completed_at: Optional[datetime] = None,
-                 exception: Optional[JsonSerializedException] = None):
-        self.id = id
-        self.query = query
+                 type_: str,
+                 max_tweets: Optional[int] = 100,
+                 batch_size: Optional[int] = None):
+        self.type = type_
         self.max_tweets = max_tweets
         self.batch_size = batch_size
+
+    def __repr__(self) -> str:
+        return type(self).__name__ + repr(self.to_json())
+
+    def __eq__(self, other: '_Work') -> bool:
+        return (type(self) == type(other)) and (self.__dict__ == other.__dict__)
+
+    @abstractmethod
+    def to_timeline(self) -> Timeline:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def to_json(self) -> Dict[str, Any]:
+        raise NotImplementedError()
+
+    @classmethod
+    @abstractmethod
+    def from_json(cls, obj: Dict[str, Any]) -> '_Work':
+        if obj['type'] == 'search':
+            return _SearchWork.from_json(obj)
+        elif obj['type'] == 'replies':
+            return _RepliesWork.from_json(obj)
+        elif obj['type'] == 'thread':
+            return _ThreadWork.from_json(obj)
+        else:
+            raise RuntimeError('Unknown work type: "{}".'.format(obj['type']))
+
+
+class _SearchWork(_Work):
+    def __init__(self,
+                 query: Search.Query,
+                 max_tweets: Optional[int],
+                 batch_size: Optional[int]):
+        super().__init__('search', max_tweets, batch_size)
+        self.query = query
+
+    def to_timeline(self) -> Search:
+        return Search(self.query, self.max_tweets, self.batch_size)
+
+    def to_json(self) -> Dict[str, Any]:
+        obj = {
+            'type': self.type,
+            'query': self.query.to_json(),
+        }
+
+        if self.max_tweets is not None:
+            obj['max_tweets'] = self.max_tweets
+        if self.batch_size is not None:
+            obj['batch_size'] = self.batch_size
+
+        return obj
+
+    @classmethod
+    def from_json(cls, obj: Dict[str, Any]) -> '_SearchWork':
+        assert obj['type'] == 'search'
+        return cls(Search.Query.from_json(obj['query']),
+                   obj.get('max_tweets'),
+                   obj.get('batch_size'))
+
+
+class _RepliesWork(_Work):
+    def __init__(self,
+                 tweet_id: str,
+                 max_tweets: Optional[int],
+                 batch_size: Optional[int]):
+        super().__init__('replies', max_tweets, batch_size)
+        self.tweet_id = tweet_id
+
+    def to_timeline(self) -> Replies:
+        return Replies(self.tweet_id, self.max_tweets, self.batch_size)
+
+    def to_json(self) -> Dict[str, Any]:
+        obj = {
+            'type': self.type,
+            'tweet_id': self.tweet_id,
+        }
+
+        if self.max_tweets is not None:
+            obj['max_tweets'] = self.max_tweets
+        if self.batch_size is not None:
+            obj['batch_size'] = self.batch_size
+
+        return obj
+
+    @classmethod
+    def from_json(cls, obj: Dict[str, Any]) -> '_RepliesWork':
+        assert obj['type'] == 'replies'
+        return cls(
+            obj['tweet_id'], obj.get('max_tweets'), obj.get('batch_size'))
+
+
+class _ThreadWork(_Work):
+    def __init__(self,
+                 tweet_id: str,
+                 max_tweets: Optional[int],
+                 batch_size: Optional[int]):
+        super().__init__('thread', max_tweets, batch_size)
+        self.tweet_id = tweet_id
+
+    def to_timeline(self) -> Thread:
+        return Thread(self.tweet_id, self.max_tweets, self.batch_size)
+
+    def to_json(self) -> Dict[str, Any]:
+        obj = {
+            'type': self.type,
+            'tweet_id': self.tweet_id,
+        }
+
+        if self.max_tweets is not None:
+            obj['max_tweets'] = self.max_tweets
+        if self.batch_size is not None:
+            obj['batch_size'] = self.batch_size
+
+        return obj
+
+    @classmethod
+    def from_json(cls, obj: Dict[str, Any]) -> '_ThreadWork':
+        assert obj['type'] == 'thread'
+        return cls(
+            obj['tweet_id'], obj.get('max_tweets'), obj.get('batch_size'))
+
+
+class _Job:
+    def __init__(self,
+                 id_: str,
+                 work: _Work,
+                 completed_at: Optional[datetime] = None,
+                 exception: Optional[JsonSerializedException] = None):
+        self.id = id_
+        self.work = work
         self.completed_at = completed_at
         self.exception = exception
 
@@ -41,20 +171,14 @@ class Job:
     def __eq__(self, other: Any) -> bool:
         return (type(self) == type(other)) and (self.__dict__ == other.__dict__)
 
-    def match(self, other: 'Job') -> bool:
-        return (self.query == other.query
-                and self.max_tweets == other.max_tweets
-                and self.batch_size == other.batch_size)
-
     def to_json(self) -> Dict[str, Any]:
-        obj = {}
-        obj['id'] = self.id
-        obj['query'] = self.query.to_json()
-        obj['max_tweets'] = self.max_tweets
-        obj['batch_size'] = self.batch_size
+        obj = {
+            'id': self.id,
+            'work': self.work.to_json(),
+        }
 
         if self.completed_at:
-            obj['completed-at'] = \
+            obj['completed_at'] = \
                 self.completed_at.strftime(NASTY_DATE_TIME_FORMAT)
 
         if self.exception is not None:
@@ -63,21 +187,19 @@ class Job:
         return obj
 
     @classmethod
-    def from_json(cls, obj: Dict[str, Any]) -> 'Job':
-        return cls(id=obj['id'],
-                   query=Search.Query.from_json(obj['query']),
-                   max_tweets=obj['max_tweets'],
-                   batch_size=obj['batch_size'],
-                   completed_at=(datetime.strptime(obj['completed-at'],
+    def from_json(cls, obj: Dict[str, Any]) -> '_Job':
+        return cls(id_=obj['id'],
+                   work=_Work.from_json(obj['work']),
+                   completed_at=(datetime.strptime(obj['completed_at'],
                                                    NASTY_DATE_TIME_FORMAT)
-                                 if 'completed-at' in obj else None),
+                                 if 'completed_at' in obj else None),
                    exception=(
                        JsonSerializedException.from_json(obj['exception'])
                        if 'exception' in obj else None))
 
 
 class Jobs:
-    def __init__(self, jobs: List[Job]):
+    def __init__(self, jobs: List[_Job]):
         self._jobs = jobs
 
     def __eq__(self, other: Any) -> bool:
@@ -101,18 +223,32 @@ class Jobs:
         logger.debug('Loading jobs from "{}".'.format(file))
 
         jobs = cls.new()
-        if file.exists():
-            with file.open('r', encoding='UTF-8') as fin:
-                for line in fin:
-                    jobs._jobs.append(Job.from_json(json.loads(line)))
+        with file.open('r', encoding='UTF-8') as fin:
+            for line in fin:
+                jobs._jobs.append(_Job.from_json(json.loads(line)))
 
         return jobs
 
-    def add_job(self,
-                query: Search.Query,
-                max_tweets: int,
-                batch_size: Optional[int] = None) -> None:
-        self._jobs.append(Job(uuid4().hex, query, max_tweets, batch_size))
+    def add_search_job(self,
+                       query: Search.Query,
+                       max_tweets: Optional[int] = 100,
+                       batch_size: Optional[int] = None) -> None:
+        self._jobs.append(
+            _Job(uuid4().hex, _SearchWork(query, max_tweets, batch_size)))
+
+    def add_replies_job(self,
+                        tweet_id: str,
+                        max_tweets: Optional[int] = 100,
+                        batch_size: Optional[int] = None) -> None:
+        self._jobs.append(
+            _Job(uuid4().hex, _RepliesWork(tweet_id, max_tweets, batch_size)))
+
+    def add_thread_job(self,
+                       tweet_id: str,
+                       max_tweets: Optional[int] = 100,
+                       batch_size: Optional[int] = None) -> None:
+        self._jobs.append(
+            _Job(uuid4().hex, _ThreadWork(tweet_id, max_tweets, batch_size)))
 
     def run(self, out_dir: Path, num_processes: int = 1) -> bool:
         logger = getLogger(__name__)
@@ -132,7 +268,7 @@ class Jobs:
         return result
 
     @classmethod
-    def _run_job(cls, job: Job, out_dir: Path) -> bool:
+    def _run_job(cls, job: _Job, out_dir: Path) -> bool:
         logger = getLogger(__name__)
         logger.info('Running job: {!r}'.format(job))
 
@@ -144,27 +280,27 @@ class Jobs:
                          'from previous run.')
 
             with meta_file.open('r', encoding='UTF-8') as fin:
-                previous_job = Job.from_json(json.load(fin))
+                previous_job = _Job.from_json(json.load(fin))
 
-            if not job.match(previous_job):
-                logger.error('  Previous job description does not match '
-                             'current one, manual intervention required! '
-                             'If the already stored data is erroneous delete '
-                             'the meta and data files of this job and rerun '
-                             'this.')
+            if job.work != previous_job.work:
+                logger.error('  Previous job work does not match current one, '
+                             'manual intervention required! If the already '
+                             'stored data is erroneous delete the meta and '
+                             'data files of this job and rerun this.')
                 logger.error('    Meta file: {!s:s}'.format(meta_file))
                 logger.error('    Data file: {!s:s}'.format(data_file))
                 return False
 
+            if previous_job.completed_at:
+                logger.debug(
+                    '  Skipping Job, because marked as completed at '
+                    '"{}".'.format(previous_job.completed_at
+                                   .strftime(NASTY_DATE_TIME_FORMAT)))
+                return True
+
             job = previous_job
             # Don't save previous exceptions back to file.
             job.exception = None
-
-            if job.completed_at:
-                logger.debug('  Skipping Job, because marked as completed at '
-                             '"{}".'.format(
-                    job.completed_at.strftime(NASTY_DATE_TIME_FORMAT)))
-                return True
 
         if data_file.exists():
             logger.info('  Deleting previously created data file "{}" because '
@@ -173,7 +309,7 @@ class Jobs:
 
         result = True
         try:
-            tweets = list(Search(job.query, job.max_tweets, job.batch_size))
+            tweets = list(job.work.to_timeline())
             with lzma.open(data_file, 'wt', encoding='UTF-8') as fout:
                 for tweet in tweets:
                     fout.write('{:s}\n'.format(json.dumps(tweet.to_json())))
--- nasty/tests/test_jobs.py
+++ nasty/tests/test_jobs.py
@@ -10,60 +10,52 @@ from uuid import uuid4
 import responses
 
 from nasty.init import init_nasty
-from nasty.jobs import Job, Jobs
+from nasty.jobs import Jobs, _Job, _RepliesWork, _SearchWork, _ThreadWork
 from nasty.search import Search
 from nasty.tests.requests_cache import RequestsCache
 from nasty.tweet import Tweet
-from nasty.util.json import JsonSerializedException
 from nasty.util.disrespect_robotstxt import disrespect_robotstxt
+from nasty.util.json import JsonSerializedException
 from nasty.util.path import TemporaryDirectoryPath, TemporaryFilePath
 
 init_nasty()
 
 
-class TestJob(unittest.TestCase):
-    def test_job_trump(self):
-        job = Job(uuid4().hex, Search.Query('trump'), 1000)
-        self.assertEqual(job, Job.from_json(job.to_json()))
-        self.assertTrue(job.match(job))
+class TestJobDumpLoad(unittest.TestCase):
+    def test_search_job_trump(self):
+        job = _Job(uuid4().hex, _SearchWork(Search.Query('trump'), 1000, None))
+        self.assertEqual(job, _Job.from_json(job.to_json()))
 
-    def test_job_trump_completed_at(self):
-        job = Job(uuid4().hex, Search.Query('trump'), 10000, 100,
-                  completed_at=datetime.now())
-        self.assertEqual(job, Job.from_json(job.to_json()))
-        self.assertTrue(
-            job.match(Job(uuid4().hex, Search.Query('trump'), 10000, 100)))
+    def test_search_job_trump_completed_at(self):
+        job = _Job(uuid4().hex, _SearchWork(Search.Query('trump'), 10000, 100),
+                   completed_at=datetime.now())
+        self.assertEqual(job, _Job.from_json(job.to_json()))
 
-    def test_job_trump_exceptions(self):
+    def test_search_job_trump_exceptions(self):
         # Collect exception with trace.
         try:
             raise ValueError('Test Error.')
         except ValueError as e:
             exception = JsonSerializedException.from_exception(e)
 
-        job = Job(uuid4().hex, Search.Query('trump'), 10, 1,
-                  exception=exception)
-        self.assertEqual(job, Job.from_json(job.to_json()))
-        self.assertTrue(
-            job.match(Job(uuid4().hex, Search.Query('trump'), 10, 1)))
+        job = _Job(uuid4().hex, _SearchWork(Search.Query('trump'), 10, 1),
+                   exception=exception)
+        self.assertEqual(job, _Job.from_json(job.to_json()))
 
-    def test_not_match(self):
-        job = Job(uuid4().hex, Search.Query('trump'), 1000, 20)
-        self.assertTrue(job.match(
-            Job(uuid4().hex, Search.Query('trump'), 1000, 20)))
-        self.assertFalse(job.match(
-            Job(uuid4().hex, Search.Query('obama'), 1000, 20)))
-        self.assertFalse(job.match(
-            Job(uuid4().hex, Search.Query('trump'), 100, 20)))
-        self.assertFalse(job.match(
-            Job(uuid4().hex, Search.Query('trump'), 1000, 2)))
+    def test_replies_job(self):
+        job = _Job(uuid4().hex, _RepliesWork('332308211321425920', None, None))
+        self.assertEqual(job, _Job.from_json(job.to_json()))
+
+    def test_thread_job(self):
+        job = _Job(uuid4().hex, _ThreadWork('332308211321425920', 123, 456))
+        self.assertEqual(job, _Job.from_json(job.to_json()))
 
 
 class TestJobsDumpLoad(unittest.TestCase):
-    def test_single_job(self):
+    def test_single_search_job(self):
         def run_test(*job_args, **job_kwargs) -> None:
             jobs = Jobs.new()
-            jobs.add_job(*job_args, **job_kwargs)
+            jobs.add_search_job(*job_args, **job_kwargs)
             with TemporaryFilePath(prefix='nasty-test-',
                                    suffix='.jsonl') as temp_file:
                 jobs.dump(temp_file)
@@ -83,18 +75,19 @@ class TestJobsDumpLoad(unittest.TestCase):
                               since=date(2009, 1, 20), until=date(2017, 1, 20)),
                  max_tweets=5, batch_size=1000)
 
-    def test_many_jobs(self):
+    def test_many_search_jobs(self):
         def run_test(num_jobs) -> None:
             jobs = Jobs.new()
             for i in range(1, num_jobs + 1):
-                jobs.add_job(Search.Query(str(i)), max_tweets=i, batch_size=i)
+                jobs.add_search_job(
+                    Search.Query(str(i)), max_tweets=i, batch_size=i)
             with TemporaryFilePath(prefix='nasty-test-',
                                    suffix='.jsonl') as temp_file:
                 jobs.dump(temp_file)
 
                 with temp_file.open('r', encoding='UTF-8') as fin:
                     lines = fin.readlines()
-                self.assertEqual(i, len(lines))
+                self.assertEqual(num_jobs, len(lines))
                 for line in lines:
                     self.assertNotEqual(0, len(line))
 
@@ -105,14 +98,14 @@ class TestJobsDumpLoad(unittest.TestCase):
         run_test(10000)
 
 
-class TestJobsRun(unittest.TestCase):
+class TestSearchJobsRun(unittest.TestCase):
     @RequestsCache()
     @disrespect_robotstxt
     def test_success(self):
         jobs = Jobs.new()
-        jobs.add_job(Search.Query('trump'), max_tweets=50)
-        jobs.add_job(Search.Query('hillary'), max_tweets=50)
-        jobs.add_job(Search.Query('obama'), max_tweets=50)
+        jobs.add_search_job(Search.Query('trump'), max_tweets=50)
+        jobs.add_search_job(Search.Query('hillary'), max_tweets=50)
+        jobs.add_search_job(Search.Query('obama'), max_tweets=50)
 
         with TemporaryDirectoryPath(prefix='nasty-test-') as temp_dir:
             self.assertTrue(jobs.run(temp_dir))
@@ -123,10 +116,10 @@ class TestJobsRun(unittest.TestCase):
     def test_success_parallel(self):
         jobs = Jobs.new()
         for i in range(16):
-            jobs.add_job(Search.Query('trump',
-                                      since=date(2019, 1, i + 1),
-                                      until=date(2019, 1, i + 2)),
-                         max_tweets=50)
+            jobs.add_search_job(Search.Query('trump',
+                                             since=date(2019, 1, i + 1),
+                                             until=date(2019, 1, i + 2)),
+                                max_tweets=50)
 
         with TemporaryDirectoryPath(prefix='nasty-test-') as temp_dir:
             self.assertTrue(jobs.run(temp_dir, num_processes=4))
@@ -138,7 +131,7 @@ class TestJobsRun(unittest.TestCase):
         # Random string that currently does not match any Tweet.
         unknown_word = 'c9dde8b5451149e683d4f07e4c4348ef'
         jobs = Jobs.new()
-        jobs.add_job(Search.Query(unknown_word), max_tweets=50)
+        jobs.add_search_job(Search.Query(unknown_word), max_tweets=50)
 
         with TemporaryDirectoryPath(prefix='nasty-test-') as temp_dir:
             self.assertTrue(jobs.run(temp_dir))
@@ -152,7 +145,7 @@ class TestJobsRun(unittest.TestCase):
     @disrespect_robotstxt
     def test_previous_match(self):
         jobs = Jobs.new()
-        jobs.add_job(Search.Query('trump'), max_tweets=50)
+        jobs.add_search_job(Search.Query('trump'), max_tweets=50)
 
         with TemporaryDirectoryPath(prefix='nasty-test-') as temp_dir:
             # Create stray (but matching) meta file.
@@ -172,7 +165,7 @@ class TestJobsRun(unittest.TestCase):
     @disrespect_robotstxt
     def test_previous_no_match(self):
         jobs = Jobs.new()
-        jobs.add_job(Search.Query('trump'), max_tweets=50)
+        jobs.add_search_job(Search.Query('trump'), max_tweets=50)
 
         with TemporaryDirectoryPath(prefix='nasty-test-') as temp_dir:
             # Run successful crawl with 'trump' and verify.
@@ -182,8 +175,8 @@ class TestJobsRun(unittest.TestCase):
             # Change job to instead query for 'obama'.
             meta_file = temp_dir / jobs._jobs[0].meta_file_name
             with meta_file.open('r', encoding='UTF-8') as fin:
-                job = Job.from_json(json.load(fin))
-            job.query.query = 'obama'
+                job = _Job.from_json(json.load(fin))
+            job.work.query.query = 'obama'
             jobs._jobs[0] = job
 
             # Verify that this fails because of job description mismatch.
@@ -198,7 +191,7 @@ class TestJobsRun(unittest.TestCase):
     @disrespect_robotstxt
     def test_previous_completed(self):
         jobs = Jobs.new()
-        jobs.add_job(Search.Query('trump'), max_tweets=50)
+        jobs.add_search_job(Search.Query('trump'), max_tweets=50)
 
         with TemporaryDirectoryPath(prefix='nasty-test-') as temp_dir:
             meta_file = temp_dir / jobs._jobs[0].meta_file_name
@@ -226,7 +219,7 @@ class TestJobsRun(unittest.TestCase):
     @disrespect_robotstxt
     def test_previous_stray_data(self):
         jobs = Jobs.new()
-        jobs.add_job(Search.Query('trump'), max_tweets=50)
+        jobs.add_search_job(Search.Query('trump'), max_tweets=50)
 
         with TemporaryDirectoryPath(prefix='nasty-test-') as temp_dir:
             # Create stray data file (with invalid data, but is irrelevant).
@@ -253,15 +246,14 @@ class TestJobsRun(unittest.TestCase):
                       status=HTTPStatus.INTERNAL_SERVER_ERROR.value)
 
         jobs = Jobs.new()
-        jobs.add_job(Search.Query('trump'), max_tweets=50)
+        jobs.add_search_job(Search.Query('trump'), max_tweets=50)
 
         with TemporaryDirectoryPath(prefix='nasty-test-') as temp_dir:
             # Run and verify that appropriate exception was logged.
             self.assertFalse(jobs.run(temp_dir))
             with (temp_dir / jobs._jobs[0].meta_file_name).open(
                     'r', encoding='UTF-8') as fin:
-                job = Job.from_json(json.load(fin))
-            print(job.exception)
+                job = _Job.from_json(json.load(fin))
             self.assertEqual(job.exception.type,
                              'UnexpectedStatusCodeException')
 
@@ -282,9 +274,9 @@ class TestJobsRun(unittest.TestCase):
             files.remove(meta_file)
 
             with meta_file.open('r', encoding='UTF-8') as fin:
-                completed_job = Job.from_json(json.load(fin))
+                completed_job = _Job.from_json(json.load(fin))
 
-            self.assertTrue(job.match(completed_job))
+            self.assertTrue(job.work == completed_job.work)
             self.assertGreater(datetime.now(), completed_job.completed_at)
             self.assertIsNone(completed_job.exception)
 
@@ -295,7 +287,7 @@ class TestJobsRun(unittest.TestCase):
             with lzma.open(data_file, 'rt', encoding='UTF-8') as fin:
                 tweets = []
                 for line in fin:
-                    self.assertIn(job.query.query, line.lower())
+                    self.assertIn(job.work.query.query, line.lower())
                     tweets.append(Tweet.from_json(json.loads(line)))
 
             if not allow_empty:
