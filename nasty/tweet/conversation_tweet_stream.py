4cd70b71e72406617f181c3bfcd771af32d45ddc
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2019-12-04 15:36:04 +0100

    Let ConversationTweetStream inherit from ABC.
    With this we do not need to relist all abstract methods of the base
    class.

--- nasty/tweet/conversation_tweet_stream.py
+++ nasty/tweet/conversation_tweet_stream.py
@@ -14,19 +14,10 @@
 # limitations under the License.
 #
 
-from abc import abstractmethod
+from abc import ABC
 
-from overrides import overrides
-
-from .tweet import Tweet
 from .tweet_stream import TweetStream
 
 
-class ConversationTweetStream(TweetStream):
-    # Repeating abstractmethod definitions of base class to not trigger
-    # PyCharm's inspection to implement abstract base methods.
-
-    @abstractmethod
-    @overrides
-    def __next__(self) -> Tweet:
-        raise NotImplementedError()
+class ConversationTweetStream(TweetStream, ABC):
+    pass
